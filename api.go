6f0be71e8418592267133d08e1f3bc01066b0c8c
hftamayo <hftamayo@gmail.com>
2023-05-21 07:07:27 -0600

    working on api requests part3

--- api.go
+++ api.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
 type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if err := f(w, r); err != nil {
+			//handle the error
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
