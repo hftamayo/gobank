8f10fa27462ff6cb0e584ca042d995eaf03bcf33
hftamayo <hftamayo@gmail.com>
2023-07-01 19:34:35 -0600

    polishing sql payloads

--- storage.go
+++ storage.go
@@ -33,6 +33,28 @@ func NewPostgresStore() (*PostgresStore, error){
 	}, nil	
 }
 
+func (s *PostgresStore) Init() error {
+	return s.createAccountTable()
+}
+
+func (s *PostgresStore) createAccountTable() error {
+	query:= `CREATE TABLE IF NOT EXISTS account (
+	id serial PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	number serial,
+	balance serial,
+	created_at timestamp
+	)`
+
+	_, err := s.db.Exec(query)
+	return err
+}
+
+
+
+
+
 func (s *PostgresStore) CreateAccount(*Account) error {
 	return nil
 }
