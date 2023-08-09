3aa5bcaedd0ad6e093da1e2485c6701478f7a414
hftamayo <hftamayo@gmail.com>
2023-08-09 15:53:45 -0600

    adding encryptedPassword property

--- types.go
+++ types.go
@@ -21,12 +21,13 @@ type CreateAccountRequest struct {
 }
 
 type Account struct {
-	ID		int `json:"id"`
-	FirstName	string `json:"firstName"`
-	LastName	string `json:"lastName"`
-	Number		int64 `json:"number"`
-	Balance 	int64 `json:"balance"`
-	CreatedAt	time.Time `json:"createdAt"`
+	ID			int		`json:"id"`
+	FirstName		string		`json:"firstName"`
+	LastName		string		`json:"lastName"`
+	Number			int64		`json:"number"`
+	EncryptedPassword	string		`json:"-"`
+	Balance			int64		`json:"balance"`
+	CreatedAt		time.Time	`json:"createdAt"`
 }
 
 func NewAccount(firstName, LastName string) *Account {
